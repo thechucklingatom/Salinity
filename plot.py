08cbbec782a683d874c111dae6ff5e273cb12741
Robert Putnam <thechucklingatom@gmail.com>
2015-04-26 15:18:26 -0600

    added labels, added salinity conversion

--- plot.py
+++ plot.py
@@ -1,14 +1,15 @@
 import serial
 import matplotlib.pyplot as plt
 import time
+import math
 from time import sleep as zzz
 import csv
 
 #serial port for the aruduino at whatever frequency it was set to
 #linux
-#ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 #windows
-ser = serial.Serial('COM3', 9600)
+#ser = serial.Serial('COM3', 9600)
 
 #gets the starting time for the program change time.clock() to time.time()
 startTime = time.time()
@@ -20,9 +21,21 @@ dataInital = [float(val) for val in line.split()][0]
 #initialize the lists and set the first value.
 timeList = []
 dataList = []
-timeList.append(startTime)
-dataList.append(dataInital* (5/1023))
+formula1List = []
+
+timeList.append(startTime - startTime)
+dataList.append(dataInital* (5/1023.0))
+if math.exp(((dataInital * (5/1023.0)) - 2.9386)/ .1525) > .26:
+    formula1List.append(.26 * 100)
+else:
+    formula1List.append(100 * math.exp(((dataInital * (5/1023.0)) - 2.9386) / .1525))
+plt.subplot(211)
+plt.ylabel("Voltage")
 plt.plot(timeList,dataList)
+plt.subplot(212)
+plt.ylabel("% Weight Salinity")
+plt.plot(timeList,formula1List)
+plt.xlabel("Time")
 plt.show(block=False)
 
 loopControl = True
@@ -56,17 +69,27 @@ with open(filename + '.csv', 'w+', newline='') as csvFile:
             while loopControl:
                 try:
                     line = ser.readline()
-                    data = [float(val) for val in line.split()][0] * (5/1023)
+                    data = [float(val) for val in line.split()][0] * (5/1023.0)
                     if data <= .5 or data > 3.4:
                         loopControl = True
                     else:
                         loopControl = False
                 except:
                     loopControl = True
-            timeList.append(time.time() - startTime)
+            timeList.append((time.time() - startTime)/60)
             dataList.append(data)
+            if math.exp((data - 2.9386) / .1525) > .26:
+                formula1List.append(.26 * 100)
+            else:
+                formula1List.append(100 * math.exp((data - 2.9386) / .1525))
             plt.clf()
+            plt.subplot(211)
+            plt.ylabel("Voltage")
             plt.plot(timeList,dataList)
+            plt.subplot(212)
+            plt.ylabel("% Weight Salinity")
+            plt.plot(timeList,formula1List)
+            plt.xlabel("Time")
             plt.draw()
             loopControl = True
             print(data)
@@ -77,9 +100,10 @@ with open(filename + '.csv', 'w+', newline='') as csvFile:
         userInput = input("enter q to quit, r to read: ")
     #read final time
     finalTime = time.time()
-    timeList.append(finalTime - startTime)
+    timeList.append((finalTime - startTime) / 60)
     #add it all to the csv file
-    testFile.writerows(zip(dataList, timeList))
-    testFile.writerow(["No data",timeList[-1]])
+    testFile.writerow(["Voltage","Time","%weight salinity"])
+    testFile.writerows(zip(dataList, timeList,formula1List))
+    testFile.writerow(["No data",timeList[-1],formula1List[-1]])
     #print running time for debug.
     print("%f:%f" % (startTime, finalTime))
